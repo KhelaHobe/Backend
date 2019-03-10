5666658d8f2ce364d42b5f40388f24eddff5bad6
FarsanRashid <farsan.rashid@gmail.com>
2019-03-10 16:19:29 +0600

    Add init for unit test folder to fix import issues

---
+++ UnitTests/__init__.py
@@ -0,0 +1,3 @@
+from sys import path
+from os.path import dirname as dir
+path.append(dir(path[0]))
--- UnitTests/test_oauth2.py
+++ UnitTests/test_oauth2.py
@@ -1,7 +1,5 @@
-"""from ..Source import oauth2
-
-
+from Source import oauth2
 def test_returns_false_for_invalid_token():
+
     gaouth_object = oauth2.GoogleOAuth()
-    assert(gaouth_object.is_valid_tokenls("token"), False)
-"""""
+    assert(gaouth_object.is_valid_token("token")) ==  False
