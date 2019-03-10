d11bfea6bcc9d0e54e2c945315d9e2f0fe79afe6
FarsanRashid <farsan.rashid@gmail.com>
2019-03-10 14:26:15 +0600

    Add testfile of oauth2

---
+++ UnitTests/test_oauth2.py
@@ -0,0 +1,7 @@
+"""from ..Source import oauth2
+
+
+def test_returns_false_for_invalid_token():
+    gaouth_object = oauth2.GoogleOAuth()
+    assert(gaouth_object.is_valid_tokenls("token"), False)
+"""""
